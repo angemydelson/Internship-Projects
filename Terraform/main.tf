e719f9a226d6da92e64802dcdf1aa65433e3e025
Angemydelson SAINT-BERT <delsonysuperdel@gmail.com>
2023-08-07 23:08:31 -0300

    :books: docs: main.tf

---
+++ Terraform/main.tf
@@ -0,0 +1,31 @@
+terraform {
+  
+}
+
+provider "aws" {
+  
+}
+
+resource "aws_instance" "vm1" {
+  
+}
+
+data "aws_ami" "image" {
+  
+}
+
+module "network" {
+  
+}
+
+variable "vm_name" {
+  
+}
+
+output "vm1_ip" {
+  
+}
+
+locals {
+  
+}
