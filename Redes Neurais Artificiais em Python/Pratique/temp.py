5127cc25064132b05904610176e1cce5c467fdcc
Angemydelson SAINT-BERT <delsonysuperdel@gmail.com>
2023-08-10 23:30:01 -0300

    :sparkles: feat: Stepfunction

---
+++ Redes Neurais Artificiais em Python/Pratique/temp.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+entradas = [-1, 7, 5]
+pesos = [0.8, 0.1, 0]
+
+def soma(e, p):
+    s = 0
+    for i in range(3):
+        #print(entradas[i])
+        #print(pesos[i])
+        s += e[i] * p[i]
+    return s
+
+s = soma(entradas, pesos)
+
+def setepFunction(soma):
+    if soma >= 1:
+        return 1
+    return 0
+
+r = setepFunction(s)
