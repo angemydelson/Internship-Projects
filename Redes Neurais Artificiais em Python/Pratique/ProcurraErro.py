0d1b01469cb7a5e0b1cd03b7cdb743a11e75d40b
Angemydelson SAINT-BERT <delsonysuperdel@gmail.com>
2023-08-14 10:07:32 -0300

    :sparkles: feat: StepfunctionOR

---
+++ Redes Neurais Artificiais em Python/Pratique/ProcurraErro.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 14 01:46:46 2023
+
+@author: angemydelson
+"""
+
+import numpy as np
+entradas = np.array([[0,0],[0,1],[1,0],[1,1]])
+saidas = np.array([0,0,0,1])
+pesos = np.array([0.0,0.0])
+taxaAprendizagem = 0.1
+
+def stepFunction(soma):
+    if soma >= 1:
+        return 1
+    return 0
+
+def calculaSaida(registro):
+    s = registro.dot(pesos)
+    return stepFunction(s)
+
+def treinar():
+    erroTotal = 1
+    while (erroTotal != 0):
+        erroTotal = 0
+        for i in range(len(saidas)):
+            saidaCalculada = calculaSaida(np.asarray(entradas[i]))
+            erro = abs(saidas[i] - saidaCalculada)
+            erroTotal += erro
+            for j in range(len(pesos)):
+                pesos[j] = pesos[j] + (taxaAprendizagem * entradas[i][j] * erro)
+                print('Peso atualizado: ' + str(pesos[j]))
+        print('Total de erro: ' + str(erroTotal))
+
+treinar()
+print("Rede neura treinada")
+print(calculaSaida(entradas[0]))
+print(calculaSaida(entradas[1]))
+print(calculaSaida(entradas[2]))
+print(calculaSaida(entradas[3]))
